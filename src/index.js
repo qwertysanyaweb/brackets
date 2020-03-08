de8d7e0fe5bc4613fcc36aa44077c9d83146a4bf
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-03-08 18:32:16 +0500

    Update scripts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strCheck = [];
+
+  bracketsConfig.forEach(element => {
+    strCheck.push(element.join(''));
+  });
+
+  for(let i=0; i < strCheck.length;){
+    if(str.includes(strCheck[i])){
+      str = str.replace(strCheck[i],'');
+      i = 0;
+    } else i++;
+  }
+
+  return (str.length == 0) ? true : false
 }
